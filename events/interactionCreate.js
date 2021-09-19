4619e65cb9f8ae2a2444ca88ecd8ebd5a2c1668b
Wisp <58921058+RomanovichLCK99@users.noreply.github.com>
2021-09-19 10:57:25 -0500

    General testing
    First tests using discord.js v13

---
+++ events/interactionCreate.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'interactionCreate',
+    once: false,
+    async execute(interaction) {
+        if (!interaction.isCommand()) return;
+
+        //Using commandName as key, gets the command from Collection commands.
+        const command = interaction.client.commands.get(interaction.commandName);
+    
+        if (!command) return;
+    
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
+}
